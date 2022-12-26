56fe879d509871c4a19156d1d860ec03ce9d0b0c
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2022-12-26 20:15:51 +0100

    Creating switchCase

--- AtmApplication.cpp
+++ AtmApplication.cpp
@@ -8,6 +8,7 @@ void showMenu()
 	cout << "[1] - Check Balance" << endl;
 	cout << "[2] - Deposit" << endl;
 	cout << "[3] - Withdraw" << endl;
+	cout << "[4] - Exit" << endl;
 	cout << endl;
 	cout << "************************" << endl;
 }
@@ -21,36 +22,37 @@ int main()
 	
 	cout << "Please choose a option: " << endl;
 	cin >> Useroption;
-	switch(Useroption)
-	{
-		case 1:
-			cout << "Your balance is: " << balance << "$" << endl;
-		break;
-		
-		case 2:
-			double depositAmount;
-			cout << "Deposit amount: ";
-			cin >> depositAmount;
-			balance += depositAmount;
-			cout << "Your new balance is: " << balance << "$" << endl;
+	do {
+		switch(Useroption)
+		{
+			case 1:
+				cout << "Your balance is: " << balance << "$" << endl;
 			break;
-		
-		case 3:
-			double withDrawMoney;
-			cout << "WithDraw amount: ";
-			cin >> withDrawMoney;
-			if(withDrawMoney <= balance)
-			{
-				balance -= withDrawMoney;
+			
+			case 2:
+				double depositAmount;
+				cout << "Deposit amount: ";
+				cin >> depositAmount;
+				balance += depositAmount;
 				cout << "Your new balance is: " << balance << "$" << endl;
-			}
-			else {
-				cout << "Not enought money" << endl;
-			}
-			break;
+				break;
 			
-			
-	}
+			case 3:
+				double withDrawMoney;
+				cout << "WithDraw amount: ";
+				cin >> withDrawMoney;
+				if(withDrawMoney <= balance)
+				{
+					balance -= withDrawMoney;
+					cout << "Your new balance is: " << balance << "$" << endl;
+				}
+				else {
+					cout << "Not enought money" << endl;
+				}
+				break;		
+		}
+	}while(Useroption == 1 || Useroption == 2 || Useroption == 3 || Useroption == 4);
+	
 	
 	return 0; 
 } 
